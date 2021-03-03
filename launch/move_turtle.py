69c84da165544e9883e37a1072a53d19dd545ead
TheAlchemista <karol12516@gmail.com>
2021-03-03 18:55:56 +0100

    Added res of necessary files

---
+++ launch/move_turtle.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package='turtlesim',
+                executable='turtlesim_node'
+            ),
+            Node(
+                package='turtle_manipulation',
+                executable='move',
+                parameters=["./config/params.yaml"]
+            )
+        ]
+    )
